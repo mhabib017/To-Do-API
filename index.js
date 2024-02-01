195831c46764214bc1b87bc073e2c3109ea43a10
Muhammad Habib <72539026+mhabib017@users.noreply.github.com>
2024-02-01 19:36:08 +0500

    changed cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const config = require("./config");
 
 const bodyParser = require("body-parser");
@@ -12,27 +13,38 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const customHeaders = {
+	"Access-Control-Allow-Headers": "Content-Type, Authorization",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Origin": "*", // Allow requests from any origin
+};
+
+// Enable CORS with custom headers
+app.use(cors({exposedHeaders: customHeaders}));
+
+
+
 const appRouter = require("./app/routes");
 
-app.use(function (req, res, next) {
-	// Website you wish to allow to connect
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	// Request methods you wish to allow
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-	// Request headers you wish to allow
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type"
-	);
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	// Pass to next layer of middleware
-	next();
-});
+// app.use(function (req, res, next) {
+// 	// Website you wish to allow to connect
+// 	res.setHeader("Access-Control-Allow-Origin", "*");
+// 	// Request methods you wish to allow
+// 	res.setHeader(
+// 		"Access-Control-Allow-Methods",
+// 		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+// 	);
+// 	// Request headers you wish to allow
+// 	res.setHeader(
+// 		"Access-Control-Allow-Headers",
+// 		"X-Requested-With,content-type"
+// 	);
+// 	// Set to true if you need the website to include cookies in the requests sent
+// 	// to the API (e.g. in case you use sessions)
+// 	res.setHeader("Access-Control-Allow-Credentials", true);
+// 	// Pass to next layer of middleware
+// 	next();
+// });
 
 app.use("*", AllRouteHandler);
 
